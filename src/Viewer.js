badbd8abf1e32f3777f39adcfd8442e5c3481214
Johan PONIN <johan.ponin.pro@gmail.com>
2022-01-14 08:54:40 +0100

    [css] fixed Viewer component bad use of semantic card html
    problem: lack of internal divs in the card dom
    
    solution: lift the docs html snippet

--- src/Viewer.js
+++ src/Viewer.js
@@ -1,24 +1,54 @@
-function Viewer ({movie, reset}) {
-    //let name = movie ? movie.name : "nothing"
-    if (movie) {
-        return (<div className="viewer">
+import { capitalizeAll } from "./prelude";
+
+function Viewer({ movie, reset }) {
+  let poster =
+    "https://dalk4zrp4jp3q.cloudfront.net/images/mac_YFVkNF/movie_placeholder_big_2x.png";
+  if (movie) {
+    let name = capitalizeAll(movie.name);
+    return (
+      <div className="viewer">
         <div className="ui card">
-        <h3>watching {movie.name}</h3>
-                <img className="playing"
-             title="click to reset"
-             onClick={() => reset()}
-             src="https://dalk4zrp4jp3q.cloudfront.net/images/mac_YFVkNF/movie_placeholder_big_2x.png"
-             alt={"movie poster for " + movie.name}></img>
+          <div className="image">
+            <img
+              className="playing"
+              title="click to reset"
+              onClick={() => reset()}
+              src={poster}
+              alt={"movie poster for " + movie.name}
+            ></img>
+          </div>
+          <div class="content">
+            <div class="header">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={"https://www.imdb.com/find?s=all&q=" + movie.name}
+              >
+                {name}
+              </a>
+            </div>
+          </div>
+          <div class="extra content">
+            <span class="right floated">{movie.genre}</span>
+            <span></span>
+          </div>
         </div>
-    </div>)
-    } else {
-        return (<div className="viewer">
-            <div className="ui card">
-        <img src="https://dalk4zrp4jp3q.cloudfront.net/images/mac_YFVkNF/movie_placeholder_big_2x.png"
-        alt="no movie"></img>
-        <h3>Nothing</h3>
-    </div></div>)
-    }
+      </div>
+    );
+  } else {
+    return (
+      <div className="viewer">
+        <div className="ui card">
+          <div className="image">
+            <img src={poster} alt="no movie"></img>
+          </div>
+          <div className="content">
+            <h3>Nothing</h3>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Viewer;
