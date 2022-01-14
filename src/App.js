023ed5794a8c498887e5db40dfc08c69cb2672a7
Johan PONIN <johan.ponin.pro@gmail.com>
2022-01-14 10:17:11 +0100

    [new][beta] testing useEffect over jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Search from './Search';
 import Movies from './Movies';
 import Viewer from './Viewer';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => { console.log('boot'); });
+
+  useEffect(() => {
+      setTimeout(() => fetch("https://jsonplaceholder.typicode.com/todos")
+		 .then(r => r.json())
+		 .then(ts => { /* console.log(ts); */ setTodos(ts); } ), 2000);
+  }, []);
   const [{search, titles, selected, viewings}, setState] = useState({
     search : "",
     history : [],
@@ -58,6 +67,10 @@ function App() {
         <Viewer reset={reset} movie={theMovie(selected)}/>
       </div>
       <div className="viewings">{viewings}</div>
+      <div className="viewings todos bottom">
+          { todos.slice(0,10).map(t => { return (<p>{t.title}</p>); }) }
+          <p>{(todos.length) ? (todos.length + " todos(s)") : "loading"}...</p>
+      </div>
       <div className="debug">
         <pre> 
           search: {search};
