99687b2194dbd5f6beb51c9d7be0bf8bd4b75d22
Johan PONIN <johan.ponin.pro@gmail.com>
2022-01-16 11:29:42 +0100

    [new][alpha] first draft of routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, ReactLocation, Link, Outlet } from 'react-location';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const location = new ReactLocation();
+
+const routes = [{
+  path: '/',
+  element: <App/>,
+},{
+  path: '/about',
+  element: <h1>About</h1>,
+},{
+  path: '/meta',
+  element: <h1>...</h1>,
+}];
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router location={location}
+	    routes={routes}>
+      <div>
+	<h1>fleetmdb</h1>
+        <Link to="/">
+          Home
+        </Link>{" "}
+        <Link to="/about">
+          About
+        </Link>{" "}
+        <Link to="/meta">
+          Meta
+        </Link>
+      </div>
+      <hr />
+      <Outlet /> 
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
