7c8403cc0c41a20af8386b69cb5ca4324f33ccf6
Johan PONIN <johan.ponin.pro@gmail.com>
2022-01-16 19:01:49 +0100

    [css] beige themed, nicer margin/padding

--- src/StatFooter.js
+++ src/StatFooter.js
@@ -1,6 +1,10 @@
 function StatFooter (prop) {
-    return (<footer>
-	  stats...
+    return (<footer className="statfooter">
+	    {[1,2,3].map(i => { return (<div><ul>
+  <li>{i}</li>
+  <li>{i}</li>
+  <li>{i}</li>
+</ul></div>) })}
 	    </footer>);
 }
 
